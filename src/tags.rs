4fd44ca195f37633b842a7b3f3a7b6937e147afb
technetos <mrgould93@gmail.com>
2021-06-06 22:56:31 -0400

    Remove redundant auth checks

--- src/tags.rs
+++ src/tags.rs
@@ -4,80 +4,63 @@ use diesel::prelude::*;
 
 /// Remove a key value pair from the tags.  
 pub fn delete(args: Args) -> Result<(), Error> {
-    if api::is_wg_and_teams(&args)? {
-        let conn = DB.get()?;
-        let key = args
-            .params
-            .get("key")
-            .ok_or("Unable to retrieve param: key")?;
+    let conn = DB.get()?;
+    let key = args
+        .params
+        .get("key")
+        .ok_or("Unable to retrieve param: key")?;
 
-        match diesel::delete(tags::table.filter(tags::key.eq(key))).execute(&conn) {
-            Ok(_) => args.msg.react(args.cx, "✅")?,
-            Err(_) => api::send_reply(&args, "A database error occurred when deleting the tag.")?,
-        }
+    match diesel::delete(tags::table.filter(tags::key.eq(key))).execute(&conn) {
+        Ok(_) => args.msg.react(args.cx, "✅")?,
+        Err(_) => api::send_reply(&args, "A database error occurred when deleting the tag.")?,
     }
     Ok(())
 }
 
 /// Add a key value pair to the tags.  
 pub fn post(args: Args) -> Result<(), Error> {
-    if api::is_wg_and_teams(&args)? {
-        let conn = DB.get()?;
+    let conn = DB.get()?;
 
-        let key = args
-            .params
-            .get("key")
-            .ok_or("Unable to retrieve param: key")?;
+    let key = args
+        .params
+        .get("key")
+        .ok_or("Unable to retrieve param: key")?;
 
-        let value = args
-            .params
-            .get("value")
-            .ok_or("Unable to retrieve param: value")?;
+    let value = args
+        .params
+        .get("value")
+        .ok_or("Unable to retrieve param: value")?;
 
-        match diesel::insert_into(tags::table)
-            .values((tags::key.eq(key), tags::value.eq(value)))
-            .execute(&conn)
-        {
-            Ok(_) => args.msg.react(args.cx, "✅")?,
-            Err(_) => api::send_reply(&args, "A database error occurred when creating the tag.")?,
-        }
-    } else {
-        api::send_reply(
-            &args,
-            "Please reach out to a Rust team/WG member to create a tag.",
-        )?;
+    match diesel::insert_into(tags::table)
+        .values((tags::key.eq(key), tags::value.eq(value)))
+        .execute(&conn)
+    {
+        Ok(_) => args.msg.react(args.cx, "✅")?,
+        Err(_) => api::send_reply(&args, "A database error occurred when creating the tag.")?,
     }
-
     Ok(())
 }
 
 /// Update an existing tag.
 pub fn update(args: Args) -> Result<(), Error> {
-    if api::is_wg_and_teams(&args)? {
-        let conn = DB.get()?;
+    let conn = DB.get()?;
 
-        let key = args
-            .params
-            .get("key")
-            .ok_or("Unable to retrieve param: key")?;
+    let key = args
+        .params
+        .get("key")
+        .ok_or("Unable to retrieve param: key")?;
 
-        let value = args
-            .params
-            .get("value")
-            .ok_or("Unable to retrieve param: value")?;
+    let value = args
+        .params
+        .get("value")
+        .ok_or("Unable to retrieve param: value")?;
 
-        match diesel::update(tags::table.filter(tags::key.eq(key)))
-            .set(tags::value.eq(value))
-            .execute(&conn)
-        {
-            Ok(_) => args.msg.react(args.cx, "✅")?,
-            Err(_) => api::send_reply(&args, "A database error occurred when updating the tag.")?,
-        }
-    } else {
-        api::send_reply(
-            &args,
-            "Please reach out to a Rust team/WG member to update a tag.",
-        )?;
+    match diesel::update(tags::table.filter(tags::key.eq(key)))
+        .set(tags::value.eq(value))
+        .execute(&conn)
+    {
+        Ok(_) => args.msg.react(args.cx, "✅")?,
+        Err(_) => api::send_reply(&args, "A database error occurred when updating the tag.")?,
     }
 
     Ok(())
